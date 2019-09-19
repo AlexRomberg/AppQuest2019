357333573937263c0d5b809bc7a1d3b4945d9458
Alex Romberg <adze@gmx.ch>
2019-09-19 06:51:54 +0200

    added editPicture

---
+++ Dechiffrierer/app/src/main/java/ch/codeclimb/dechiffrierer/editPicture.java
@@ -0,0 +1,14 @@
+package ch.codeclimb.dechiffrierer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_picture);
+    }
+}
