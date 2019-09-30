63a17876f00f2620761671af98a62547aafc1d9f
Alex Romberg <adze@gmx.ch>
2019-09-30 22:40:13 +0200

    added editPicture Function edited comments

--- Dechiffrierer/app/src/main/java/ch/codeclimb/dechiffrierer/MainActivity.java
+++ Dechiffrierer/app/src/main/java/ch/codeclimb/dechiffrierer/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -13,32 +12,39 @@ import android.widget.Toast;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //global views
     Button btLog, btNewPicture;
     EditText tbInput;
     ImageView ivPicture;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Bitmap bmPicture;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1, REQUEST_IMAGE_EDIT = 2;
 
+    //is executed when app gets loaded
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializes Views
         btLog = findViewById(R.id.btLog);
         btNewPicture = findViewById(R.id.btNewPicture);
         tbInput = findViewById(R.id.tbInput);
         ivPicture = findViewById(R.id.imageView);
     }
 
+    //is executed when app gets started
     protected void onResume() {
         super.onResume();
 
         //Adds eventlisteners
         btLog.setOnClickListener(this);
         btNewPicture.setOnClickListener(this);
+        ivPicture.setOnClickListener(this);
     }
 
-    //Sends string to logbuch application
+    //Sends string to logbuch-application
     private void log(String qrCode) {
         Intent intent = new Intent("ch.appquest.intent.LOG");
 
@@ -52,17 +58,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
+    //Is executed when a view gets clicked
     @Override
     public void onClick(View v) {
         if (v.getId() == btLog.getId()) {
+
+            //"log" button pressed
             String logMessage = tbInput.getText().toString();
             log(logMessage);
             tbInput.setText("");
+
         } else if (v.getId() == btNewPicture.getId()) {
+
+            //"new picture" button pressed
             dispatchTakePictureIntent();
+
+        } else if (v.getId() == ivPicture.getId()) {
+
+            //imageview pressed
+            onPictureClicked();
+
         }
     }
 
+    //Requests an Image from camera-application
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -70,33 +89,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //Is executed when intetnt replies
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            //new picture intent
             Bundle extras = data.getExtras();
-            Bitmap bmPicture = applyFilter((Bitmap) extras.get("data"));
+            bmPicture = applyFilter((Bitmap) extras.get("data"));
             ivPicture.setImageBitmap(bmPicture);
+
+        } else if (requestCode == REQUEST_IMAGE_EDIT && resultCode == RESULT_OK) {
+
+            //edit picture intent
+            Bundle extras = data.getExtras();
+            bmPicture = (Bitmap) extras.get("RETURNIMAGE");
+            ivPicture.setImageBitmap(bmPicture);
+
         }
     }
 
+
+    //takes green and blue value out of image
     private Bitmap applyFilter(Bitmap bitmap) {
+
+        //takes image dimensions
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
-        int[] data = new int[width * height];
 
+        //creates array and fills pixels in it
+        int[] data = new int[width * height];
         bitmap.getPixels(data, 0, width, 0, 0, width, height);
 
+        //edits evey pixel
         for (int i = 0; i < data.length; i++) {
-            int A = (data[i] >> 24) & 0xff; // or color >>> 24
+
+            //converts ARGB-value to A, R, G, B 255 values
+            int A = (data[i] >> 24) & 0xff;
             int R = (data[i] >> 16) & 0xff;
-            int G = (data[i] >> 8) & 0xff;
-            int B = (data[i]) & 0xff;
-            G = 0;
-            B = 0;
+            int G = R;
+            int B = R;
+
+            //converts new 255 values back to ARGB-value
             data[i] = (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 8 | (B & 0xff);
-
         }
-
+        //creates bitmap
         return Bitmap.createBitmap(data, width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    //opens bitmap-edit activity
+    public void onPictureClicked() {
+        if (bmPicture != null) {
+            Intent intent = new Intent(this, editPicture.class);
+            intent.putExtra("IMAGE", bmPicture);
+            startActivityForResult(intent, REQUEST_IMAGE_EDIT);
+        }
     }
 }
--- Dechiffrierer/app/src/main/java/ch/codeclimb/dechiffrierer/editPicture.java
+++ Dechiffrierer/app/src/main/java/ch/codeclimb/dechiffrierer/editPicture.java
@@ -2,13 +2,102 @@ package ch.codeclimb.dechiffrierer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
-public class editPicture extends AppCompatActivity {
+public class editPicture extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
+    //global views
+    Bitmap bmPictureOriginal, bmPictureFinished;
+    ImageView ivPicture;
+    SeekBar sbBrightnes;
+    Button btBack;
+
+    //is executed when app gets loaded
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_picture);
+
+        //initializes Views
+        ivPicture = findViewById(R.id.ivPicture);
+        sbBrightnes = findViewById(R.id.sbBrightnes);
+        btBack = findViewById(R.id.btBack);
+
+        //gets Image from Intent and sets it in ivPicture
+        Intent intent = getIntent();
+        bmPictureOriginal = intent.getExtras().getParcelable("IMAGE");
+        bmPictureFinished = bmPictureOriginal;
+        ivPicture.setImageBitmap(bmPictureOriginal);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        //Adds eventlisteners
+        sbBrightnes.setOnSeekBarChangeListener(this);
+        btBack.setOnClickListener(this);
+    }
+
+    private Bitmap applyFilter(Bitmap bitmap, int brightnes) {
+        //takes image dimensions
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        //creates array and fills pixels in it
+        int[] data = new int[width * height];
+        bitmap.getPixels(data, 0, width, 0, 0, width, height);
+
+        //edits evey pixel
+        for (int i = 0; i < data.length; i++) {
+            //converts ARGB-value to A, R, G, B 255 values
+            int A = (data[i] >> 24) & 0xff;
+            int R = (data[i] >> 16) & 0xff;
+
+            //reduces brightness
+            R -= (255 - brightnes);
+            if (R < 0) {
+                R = 0;
+            }
+
+            //converts new 255 values back to ARGB-value
+            data[i] = (A & 0xff) << 24 | (R & 0xff) << 16 | (R & 0xff) << 8 | (R & 0xff);
+        }
+        //creates bitmap
+        return Bitmap.createBitmap(data, width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    //apples filter to image if Progressbar changed
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        bmPictureFinished = applyFilter(bmPictureOriginal, progress);
+        ivPicture.setImageBitmap(bmPictureFinished);
+    }
+
+    //returns Image
+    @Override
+    public void onClick(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("RETURNIMAGE", bmPictureFinished);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+
+    //without function
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    //without function
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 }
