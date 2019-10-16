09d78caa53b00513cd553a2f09e4668c3012134e
Alex Romberg <adze@gmx.ch>
2019-10-16 16:59:03 +0200

    added auto Imageview

--- Memory/app/src/main/java/ch/climbcode/memory/MainActivity.java
+++ Memory/app/src/main/java/ch/climbcode/memory/MainActivity.java
@@ -2,22 +2,44 @@ package ch.climbcode.memory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
 
-public class MainActivity extends AppCompatActivity  {
-    LinearLayout contentGroups [] = new LinearLayout [1];
+import java.io.LineNumberReader;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout llContentGroupHolder;
+    LinearLayout contentGroups[] = new LinearLayout[1];
+    LinearLayout imageTextGroups[][] = new LinearLayout[1][2];
+    ImageView images[][] = new ImageView[1][2];
+    TextView texts[][] = new TextView[1][2];
 
     //is started when app gets created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        creatContentGroup();
+
+        llContentGroupHolder = findViewById(R.id.llContentGroupHolder);
+        createContentGroup();
     }
 
     public void takeQrCodePicture() {
@@ -29,9 +51,21 @@ public class MainActivity extends AppCompatActivity  {
         integrator.initiateScan();
     }
 
-    public void creatContentGroup(){
+    public void createContentGroup() {
+        for (int i = 0; i < 2; i++) {
+            ImageView ivImage = new ImageView(this);
+            ivImage.setLayoutParams(new LinearLayout.LayoutParams(Resources.getSystem().getDisplayMetrics().widthPixels/2, ViewGroup.LayoutParams.MATCH_PARENT));
+            ivImage.setImageResource(R.drawable.plus);
+            images[0][i] = ivImage;
+            contentGroups[0].addView(images[0][i]);
+        }
+        contentGroups[0]= new LinearLayout(this);
+        contentGroups[0].setOrientation(LinearLayout.HORIZONTAL);
+        contentGroups[0].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300));
+        llContentGroupHolder.addView(contentGroups[0]);
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == IntentIntegrator.REQUEST_CODE
