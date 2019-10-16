4809b890aede952ee1fc08367d8c9b50bd44edc1
Alex Romberg <adze@gmx.ch>
2019-10-16 14:40:02 +0200

    added given

--- Memory/app/src/main/java/ch/climbcode/memory/MainActivity.java
+++ Memory/app/src/main/java/ch/climbcode/memory/MainActivity.java
@@ -2,13 +2,43 @@ package ch.climbcode.memory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
 
+public class MainActivity extends AppCompatActivity  {
+
+
+    //is started when app gets created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        //integrator.setCaptureActivity(MyCaptureActivity.class);
+        //integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE
+                && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            String path = extras.getString(Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
+
+            // Ein Bitmap zur Darstellung erhalten wir so:
+            // Bitmap bmp = BitmapFactory.decodeFile(path)
+
+            String code = extras.getString(Intents.Scan.RESULT);
+        }
     }
 }
